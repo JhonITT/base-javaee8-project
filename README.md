eded40f5b7455674ed0b27b368767ef0d8b12fa9
Jhon Marlon Fernandez Almanzar <JmFernandezA@edenorte.com.do>
2018-03-07 14:51:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#	base-javaee8-project
+This is a Java EE 8 project to start from scratch.
+
+## Tools
+| Tool | Version |
+| ------ | ------ |
+| NETBEANS |  8.2 |
+| JAVA_JDK | 1.8.0_151 |
+| GLASSFISH | 5.0|
+| JSF | 2.3.3 |
+| MAVEN | 3.0.5|
---
+++ src/main/java/com/javaee8examples/basejavaee8/Jsf23Activator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaee8examples.basejavaee8;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author JmFernandezA
+ */
+
+
+@ApplicationScoped
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class Jsf23Activator {
+
+}
